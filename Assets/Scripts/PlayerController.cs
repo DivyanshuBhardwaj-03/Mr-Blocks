6b459790fb551f5112ea058848a6d778047be2e7
DivyanshuBhardwaj-03 <divyanshubhardwaj013@gmail.com>
2022-04-20 13:54:52 +0530

    3rd Commit (UI added in the Project)
    UI for game End and Pause/resume added in the project.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D rigidbody2d;
     public float speed;
+    public GameObject gameWonPanel;
+    public GameObject gamePausePanel;
+    private bool isGameWon = false;
+    private bool isGamePaused = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Code to Pause/Resume Game on Press of Esc key
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isGamePaused == true)
+            {
+                isGamePaused = false;
+                gamePausePanel.SetActive(false);
+            }
+            else if (isGamePaused == false)
+            {
+                isGamePaused = true;
+                gamePausePanel.SetActive(true);
+               
+            }
+            
+        }
+        if(isGameWon == true || isGamePaused == true)
+        {
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigidbody2d.velocity = new Vector2(0f, 0f);
@@ -54,6 +78,8 @@ public class PlayerController : MonoBehaviour
         if(collision.tag == "Door")
         {
             Debug.Log("Level Complete!!!");
+            gameWonPanel.SetActive(true);
+            isGameWon = true;
         }
         
     }
